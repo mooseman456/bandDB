dfb58ec51e83af6960da71a12525a68d4dc01037
moosemn456 <jgstangelo@gmail.com>
2013-10-20 14:52:13 -0500

    Added stuff

--- populate.php
+++ populate.php
@@ -6,13 +6,13 @@ function importDataToDB()
   $con = mysql_connect("localhost", "root", "srsbizman456") or die ("Could not connect: " . mysql_error());
   mysql_select_db("assignment2", $con) or die ("Could not connect: " . mysql_error());
 
-  $json_string = 'http://54.200.82.84/Kustom-Kupcake/data/menu.json';
+  $json_string = 'http://54.200.112.242/bandDB/bands.json';
   $json = file_get_contents($json_string);
   
   $result = json_decode($json, TRUE);
   
   $i = 1;
-  $cake = $result['menu']['cakes'];
+  $bands = $result['menu']['cakes'];
   $frosting = $result['menu']['frosting'];
   $fillings = $result['menu']['fillings'];
   $toppings = $result['menu']['Toppings'];
