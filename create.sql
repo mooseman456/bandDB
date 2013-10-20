a65d50f754e28737797f799447466fc6a4237334
moosemn456 <jgstangelo@gmail.com>
2013-10-20 15:26:53 -0500

    Stuff

---
+++ create.sql
@@ -0,0 +1,31 @@
+DROP DATABASE IF EXISTS bandDB;
+CREATE DATABASE bandDB;
+USE bandDB;
+
+create table bands (
+   name varchar(50),
+   startDate varchar(50),
+   endDate varchar(50),
+   vocals varchar(50),
+   guitar varchar(50),
+   keybaord varchar(50),
+   synthesizer varchar(50),
+   bass varchar(50),
+   drums varchar(50),
+   percussion varchar(50),
+   backingVocals varchar(50),
+   rhythmGuitar varchar(50),
+   bandID int AUTO_INCREMENT,
+   PRIMARY KEY(bandID)
+) engine = InnoDB;
+
+create table players (
+   name varchar(50),
+   birthDate varchar(50),
+   deathDate varchar(50),
+   city varchar(50),
+   state varchar(50),
+   country varchar(50),
+   playerID int AUTO_INCREMENT,
+   PRIMARY KEY(playerID)
+) engine = InnoDB;
--- populate.php
+++ populate.php
@@ -4,48 +4,33 @@ function importDataToDB()
 {
   echo("<html><body>");
   $con = mysql_connect("localhost", "root", "srsbizman456") or die ("Could not connect: " . mysql_error());
-  mysql_select_db("assignment2", $con) or die ("Could not connect: " . mysql_error());
+  mysql_select_db("assignment2", $con) or die ("Could not connect: " . mysqli_error());
 
   $json_string = 'http://54.200.112.242/bandDB/bands.json';
   $json = file_get_contents($json_string);
   
   $result = json_decode($json, TRUE);
   
-  $i = 1;
-  $bands = $result['menu']['cakes'];
-  $frosting = $result['menu']['frosting'];
-  $fillings = $result['menu']['fillings'];
-  $toppings = $result['menu']['Toppings'];
-  foreach($cake as $value) 
+  $i = 0;
+  $bands = $result['bands'];
+  foreach($bands as $value) 
   {
-	  mysql_query("INSERT INTO flavor (flavorID, flavorName, picLoc) VALUES (".$i.",\"" 
-	  .$value['flavor']."\",\"" 
-	  .$value['img_url']."\");");
+	  mysql_query("INSERT INTO bands VALUES (".$value[$i]['Band'].",\"" 
+	  .$value[$i]['startDate']."\",\"" 
+	  .$value[$i]['endDate']."\",\""
+	  .$value[$i]['Vocals']."\",\"" 
+	  .$value[$i]['Guitar']."\",\"" 
+	  .$value[$i]['Keyboard']."\",\"" 
+	  .$value[$i]['Synthesizer']."\",\"" 
+	  .$value[$i]['Bass']."\",\"" 
+	  .$value[$i]['Drums']."\",\"" 
+	  .$value[$i]['Percussion']."\",\"" 
+	  .$value[$i]['Backing Vocals']."\",\"" 
+	  .$value[$i]['Rhythm Guitar']."\");"); 
+	  
 	  $i = $i + 1;
   }
-  $i = 1;
-  foreach($frosting as $value) 
-  {
-	  mysql_query("INSERT INTO icing (icingID, icingName, picLoc) VALUES (".$i.",\"" 
-	  .$value['flavor']."\",\"" 
-	  .$value['img_url']."\");");
-	  $i = $i + 1;
-  }
-  $i = 1;
-  foreach($fillings as $value) 
-  {
-	  mysql_query("INSERT INTO filling (fillingID, fillingName, rgbVal) VALUES (".$i.",\"" 
-	  .$value['flavor']."\",\"" 
-	  .$value['rgb']."\");");
-	  $i = $i + 1;
-  }
-  $i = 1;
-  foreach($toppings as $value) 
-  {
-	  mysql_query("INSERT INTO topping (toppingID, toppingName) VALUES (".$i.",\"" 
-	  .$value."\");");
-	  $i = $i + 1;
-  }
+  
   mysql_close($con);
   /*include 'simplexlsx.class.php';
   $xlsx = new SimpleXLSX('http://54.200.82.84/Kustom-Kupcake/data/CustomCupcakesDBData-FavoriteCupcakes.xlsx');
