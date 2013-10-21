1af17be2c87e64e06ac338efbd2776a165e76dc0
moosemn456 <jgstangelo@gmail.com>
2013-10-20 23:23:42 -0500

    S

--- bandget.php
+++ bandget.php
@@ -9,25 +9,17 @@
             
             $result = mysql_query("select distinct(players.name), players.birthDate, deathDate, city, state, country, playerID from players, bands where (players.name = bands.vocals OR players.name = bands.drums OR players.name = bands.guitar OR players.name = bands.keybaord OR players.name = bands.synthesizer OR players.name = bands.bass OR players.name = bands.percussion OR players.name = backingVocals OR players.name = bands. rhythmGuitar) AND (bands.startDate <=".$year." AND (bands.endDate = 0 OR bands.endDate >= ".$year.")) AND (bands.name = \"".$name."\");");
             
-            echo("{ <br/>
-                  \"members\":[ <br/>");
+            $JSON = "{\"members\":[";
             $i = mysql_num_rows($result);
             for($x = 0; $x < $i; $x++)
             {
                $getMembers = mysql_fetch_array($result);
-               echo("{\"name\": \"".$getMembers['name']."\",
-                      \"birthDate\": \"".$getMembers['birthDate']."\",
-                      \"deathDate\": \"".$getMembers['deathDate']."\",
-                      \"city\": \"".$getMembers['city']."\",
-                      \"state\": \"".$getMembers['state']."\",
-                      \"country\": \"".$getMembers['country']."\"}");
+               $JSON .= "{\"name\": \"".$getMembers['name']."\",\"birthDate\": \"".$getMembers['birthDate']."\",\"deathDate\": \"".$getMembers['deathDate']."\",\"city\": \"".$getMembers['city']."\",\"state\": \"".$getMembers['state']."\",\"country\": \"".$getMembers['country']."\"}";
                if ($x+1 != $i)
-                  echo(", <br/><br/>");
-               else
-                  echo("<br/><br/>");   
-               
+                  $JSON .= ", ";               
             }
-            echo("]<br/>}");
+            $JSON .= "]}";
+            echo($JSON);
          }
          mysql_close($con);
       ?>
