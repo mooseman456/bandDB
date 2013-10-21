0c52d98a3b726f623d76f781c930301e6b9466d8
moosemn456 <jgstangelo@gmail.com>
2013-10-20 23:01:16 -0500

    More stuff

---
+++ bandget.php
@@ -0,0 +1,35 @@
+<html>
+   <body>
+      <?         
+         $con = mysql_connect("localhost", "root", "srsbizman456") or die ("Could not connect: ".mysql_error());
+         mysql_select_db("assignment2", $con) or die ("Could not connect: " . mysqli_error());
+         if(isset($_GET['name'])){
+            $name = $_GET['name'];
+            $year = $_GET['year'];
+            
+            $result = mysql_query("select distinct(players.name), players.birthDate, deathDate, city, state, country, playerID from players, bands where (players.name = bands.vocals OR players.name = bands.drums OR players.name = bands.guitar OR players.name = bands.keybaord OR players.name = bands.synthesizer OR players.name = bands.bass OR players.name = bands.percussion OR players.name = backingVocals OR players.name = bands. rhythmGuitar) AND (bands.startDate <=".$year." AND (bands.endDate = 0 OR bands.endDate >= ".$year.")) AND (bands.name = \"".$name."\");");
+            
+            echo("{ <br/>
+                  \"members\":[ <br/>");
+            $i = mysql_num_rows($result);
+            for($x = 0; $x < $i; $x++)
+            {
+               $getMembers = mysql_fetch_array($result);
+               echo("{\"name\": \"".$getMembers['name']."\",
+                      \"birthDate\": \"".$getMembers['birthDate']."\",
+                      \"deathDate\": \"".$getMembers['deathDate']."\",
+                      \"city\": \"".$getMembers['city']."\",
+                      \"state\": \"".$getMembers['state']."\",
+                      \"country\": \"".$getMembers['country']."\"}");
+               if ($x+1 != $i)
+                  echo(", <br/><br/>");
+               else
+                  echo("<br/><br/>");   
+               
+            }
+            echo("]<br/>}");
+         }
+         mysql_close($con);
+      ?>
+   </body>
+</html>
